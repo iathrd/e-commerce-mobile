f417b698a6824f6a4f83ba78fcc54a874ee40d93
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-13 20:30:47 +0800

    created my orders screen

---
+++ src/screens/MyOrders.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Container,Content,Text,Card } from 'native-base'
+
+export default function MyOrders() {
+    return (
+        <Container>
+            <Content style={{paddingLeft:10,paddingRight:10,backgroundColor:"#f5f5f5"}}>
+                <Text style={{fontSize: 35, fontWeight: 'bold'}}>My orders</Text>
+                <Card style={{ borderColor:"#f5f5f5",padding:20,borderRadius:10,display:"flex",flexDirection:"column",marginTop:30,marginBottom:20}}>
+                <View style={{display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"space-between"}}>
+                    <Text>Order No1947034</Text>
+                    <Text style={{color:"grey"}}>05-12-2019</Text>
+                </View>
+                <View style={{marginTop:15}}>
+                    <Text style={{color:"grey"}}>Tracking number: <Text style={{fontWeight:"bold"}}>IW3475453455</Text></Text>
+                    <Text style={{color:"grey",marginTop:10}}>Quantity: <Text style={{fontWeight:"bold"}}>3</Text></Text>
+                    <Text style={{color:"grey",marginTop:10}}>Total Amount: <Text style={{fontWeight:"bold"}}>112$</Text></Text>
+                    <View style={{alignSelf:"flex-end"}}>
+                        <Text style={{fontWeight:"bold",color:"#37de45"}}>Delivered</Text>
+                    </View>
+                </View>
+                </Card>
+
+                <Card style={{ borderColor:"#f5f5f5",padding:20,borderRadius:10,display:"flex",flexDirection:"column",marginBottom:20}}>
+                <View style={{display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"space-between"}}>
+                    <Text>Order No1947034</Text>
+                    <Text style={{color:"grey"}}>05-12-2019</Text>
+                </View>
+                <View style={{marginTop:15}}>
+                    <Text style={{color:"grey"}}>Tracking number: <Text style={{fontWeight:"bold"}}>IW3475453455</Text></Text>
+                    <Text style={{color:"grey",marginTop:10}}>Quantity: <Text style={{fontWeight:"bold"}}>3</Text></Text>
+                    <Text style={{color:"grey",marginTop:10}}>Total Amount: <Text style={{fontWeight:"bold"}}>112$</Text></Text>
+                    <View style={{alignSelf:"flex-end"}}>
+                        <Text style={{fontWeight:"bold",color:"#37de45"}}>Delivered</Text>
+                    </View>
+                </View>
+                </Card>
+                <Card style={{ borderColor:"#f5f5f5",padding:20,borderRadius:10,display:"flex",flexDirection:"column",marginBottom:20}}>
+                <View style={{display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"space-between"}}>
+                    <Text>Order No1947034</Text>
+                    <Text style={{color:"grey"}}>05-12-2019</Text>
+                </View>
+                <View style={{marginTop:15}}>
+                    <Text style={{color:"grey"}}>Tracking number: <Text style={{fontWeight:"bold"}}>IW3475453455</Text></Text>
+                    <Text style={{color:"grey",marginTop:10}}>Quantity: <Text style={{fontWeight:"bold"}}>3</Text></Text>
+                    <Text style={{color:"grey",marginTop:10}}>Total Amount: <Text style={{fontWeight:"bold"}}>112$</Text></Text>
+                    <View style={{alignSelf:"flex-end"}}>
+                        <Text style={{fontWeight:"bold",color:"#37de45"}}>Delivered</Text>
+                    </View>
+                </View>
+                </Card>
+            </Content>
+        </Container>
+    )
+}
