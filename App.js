a2fc5f4d0c6e49217bfc8b19ead3a2c2d4802375
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-13 21:26:08 +0800

    fixed code (eslint)

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
-import React, {useEffect} from 'react'
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 
 import store from './src/redux/store';
 
 import Main from './src/screens/Main';
-import RNBootSplash from "react-native-bootsplash";
+import RNBootSplash from 'react-native-bootsplash';
 
-export default function App () {
+export default function App() {
   let init = async () => {
     // …do multiple async tasks
   };
-  useEffect(()=>{
-    init().finally(()=>{
-      RNBootSplash.hide({duration:200})
-    })
-  },[])
-    return (
-      <Provider store={store}>
-        <Main />
-      </Provider>
-    );
+  useEffect(() => {
+    init().finally(() => {
+      RNBootSplash.hide({duration: 200});
+    });
+  }, []);
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
 }
