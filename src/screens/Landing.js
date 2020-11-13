aa19c74f7f95662c2c681b9b61e587d012cb863b
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-13 20:29:51 +0800

    created landing screen

---
+++ src/screens/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View,StyleSheet } from 'react-native'
+import {Text,Container,Content,Button} from 'native-base'
+
+export default function Landing() {
+    return (
+        <Container>
+            <Content>
+                <View>
+                    <Text>Landing</Text>
+                </View>
+            </Content>
+        </Container>
+    )
+}
