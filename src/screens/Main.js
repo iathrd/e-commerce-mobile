eff7cf7adf51e667517414aaf13e3c45ccec17f9
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-13 20:34:04 +0800

    created bottom tab avigator

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,28 +1,67 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
-import {useSelector} from 'react-redux'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-const Stack = createStackNavigator()
-const BottomTab = createBottomTabNavigator()
+import {useSelector} from 'react-redux';
+const BottomTab = createBottomTabNavigator();
 
-import Login from './Login'
-import SignUp from './SignUp'
-import ForgotPassword from './ForgotPassword'
-import ResetPassword from './ResetPassword'
-import Home from './Home'
+import Login from './Login';
+import SignUp from './SignUp';
+import ForgotPassword from './ForgotPassword';
+import ResetPassword from './ResetPassword';
+import Home from './Home';
+import Mybag from './MyBag';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {HomeStack,BagStack,MyProfileStack} from './StackNavigator'
+import Landing from './Landing'
+
 export default function App() {
-    const auth = useSelector(state=>state.auth)
+  const auth = useSelector((state) => state.auth);
   return (
     <NavigationContainer>
-        {!auth.isLogin ? (
-            <Stack.Navigator>
-            <Stack.Screen options={{headerShown:false}} name="Login" component={Home}/>
+      {!auth.isLogin ? (
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Landing"
+            component={Landing}
+          />
         </Stack.Navigator>
-        ):""}
+      ) : (
+        <BottomTab.Navigator>
+          <BottomTab.Screen
+            options={{
+              tabBarIcon: ({size, color, focused}) => (
+                <Icon name="home-outline" size={30} color={color} />
+              ),
+            }}
+            name="Home"
+            component={HomeStack}
+          />
+          <BottomTab.Screen  options={{
+              tabBarIcon: ({size, color, focused}) => (
+                <Icon name="cart-outline" size={30} color={color} />
+              ),
+            }} name="Shop" component={Login} />
+          <BottomTab.Screen  options={{
+              tabBarIcon: ({size, color, focused}) => (
+                <Icon name="shopping-outline" size={30} color={color} />
+              ),
+            }} name="Bag" component={BagStack} />
+            <BottomTab.Screen  options={{
+              tabBarIcon: ({size, color, focused}) => (
+                <Icon name="heart-outline" size={30} color={color} />
+              ),
+            }} name="Favorit" component={Mybag} />
+            <BottomTab.Screen  options={{
+              tabBarIcon: ({size, color, focused}) => (
+                <Icon name="account-outline" size={30} color={color} />
+              ),
+            }} name="Profile" component={MyProfileStack} />
+        </BottomTab.Navigator>
+      )}
     </NavigationContainer>
-  )
+  );
 }
