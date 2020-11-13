05954a4a29b308a0ac3fe7167bf018650ff39070
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-13 21:24:59 +0800

    created splash screen

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import React, {Component} from '
+import React, {useEffect} from 'react'
 import {Provider} from 'react-redux';
 
 import store from './src/redux/store';
 
 import Main from './src/screens/Main';
+import RNBootSplash from "react-native-bootsplash";
 
-export default class App extends Component {
-  render() {
+export default function App () {
+  let init = async () => {
+    // …do multiple async tasks
+  };
+  useEffect(()=>{
+    init().finally(()=>{
+      RNBootSplash.hide({duration:200})
+    })
+  },[])
     return (
       <Provider store={store}>
         <Main />
       </Provider>
     );
-  }
 }
