39016f300a6941904a44c8746296b519e793ea64
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-13 20:37:17 +0800

    changes

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import {Provider} from 'react-redux'
+import React, {Component} from '
+import {Provider} from 'react-redux';
 
-import store from './src/redux/store'
+import store from './src/redux/store';
 
-import Main from './src/screens/Main'
+import Main from './src/screens/Main';
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <Main/>
+        <Main />
       </Provider>
-    )
+    );
   }
 }
