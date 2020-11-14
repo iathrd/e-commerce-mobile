0579ae22862193373e7840732eca2ebedaf5c2c2
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-14 14:50:20 +0800

    added form validation

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,43 +1,71 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import {
-  Input,
-  Form,
-  Item,
-  Button,
-  Label,
-  Content,
-  Text,
-  Icon,
-} from 'native-base';
+import {Formik} from 'formik';
+import {Input, Item, Button, Label, Content, Text} from 'native-base';
+import {loginValidationSchema} from '../helpers/formValidation';
 
 export default function SignUp() {
   return (
     <>
       <Content style={{paddingLeft: 10, paddingRight: 10}}>
-        <Form>
-          <View>
-            <Text style={styles.loginText}>Login</Text>
-          </View>
-          <View style={styles.paper}>
-            <Item style={styles.formInput} floatingLabel>
-              <Label>Email</Label>
-              <Input placeholder="Email"  />
-            </Item>
-          </View>
-          <View style={styles.paper}>
-            <Item style={styles.formInput} floatingLabel>
-              <Label>Password</Label>
-              <Input placeholder="Password" />
-            </Item>
-          </View>
-          <View style={{alignSelf:"flex-end"}}>
-            <Text>Forgot your password?</Text>
-          </View>
-          <Button style={styles.btnSignUp} block>
-            <Text style={styles.btnText}>Login</Text>
-          </Button>
-        </Form>
+        <Formik
+          validationSchema={loginValidationSchema}
+          initialValues={{email: '', password: ''}}
+          onSubmit={(values) => console.log(values)}>
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+            isValid,
+          }) => (
+            <>
+              <View>
+                <Text style={styles.loginText}>Login</Text>
+              </View>
+              <View style={styles.paper}>
+                <Item style={styles.formInput} floatingLabel>
+                  <Label>Email</Label>
+                  <Input
+                    placeholder="Email"
+                    name="email"
+                    onChangeText={handleChange('email')}
+                    onBlur={handleBlur('email')}
+                    value={values.email}
+                    keyboardType="email-address"
+                  />
+                </Item>
+                {errors.email && touched.email && (
+                  <Text style={{color: 'red'}}>{errors.email}</Text>
+                )}
+              </View>
+
+              <View style={styles.paper}>
+                <Item style={styles.formInput} floatingLabel>
+                  <Label>Password</Label>
+                  <Input
+                    placeholder="Password"
+                    name="password"
+                    onChangeText={handleChange('password')}
+                    onBlur={handleBlur('password')}
+                    value={values.password}
+                  />
+                </Item>
+                {errors.password && touched.password && (
+                  <Text style={{color: 'red'}}>{errors.password}</Text>
+                )}
+              </View>
+              <View style={{alignSelf: 'flex-end'}}>
+                <Text>Forgot your password?</Text>
+              </View>
+              <Button onPress={handleSubmit} style={styles.btnSignUp} block>
+                <Text style={styles.btnText}>Login</Text>
+              </Button>
+            </>
+          )}
+        </Formik>
       </Content>
     </>
   );
@@ -56,7 +84,7 @@ const styles = StyleSheet.create({
   btnSignUp: {
     borderRadius: 25,
     marginTop: 30,
-    backgroundColor:"#3285A8"
+    backgroundColor: '#3285A8',
   },
   btnText: {
     fontSize: 17,
@@ -66,19 +94,18 @@ const styles = StyleSheet.create({
   loginText: {
     fontSize: 35,
     fontWeight: 'bold',
-    marginBottom:80
+    marginBottom: 80,
   },
   formInput: {
-   borderColor: 'transparent',
-    borderRadius:10,
-    paddingBottom:25,
-
+    borderColor: 'transparent',
+    borderRadius: 10,
+    paddingBottom: 25,
   },
-  paper:{
-    backgroundColor:"white",
-    borderRadius:10,
-    elevation:1.5,
-    marginBottom:10
+  paper: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    elevation: 1.5,
+    marginBottom: 10,
   },
   labelText: {
     fontSize: 12,
