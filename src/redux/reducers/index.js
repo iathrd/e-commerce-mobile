16e4f674749c4c5cf4dbb9ba89b94ae533964d24
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-14 14:53:31 +0800

    added formik and yup

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,46 +1,44 @@
-import {combineReducers} from 'redux'
+import {combineReducers} from 'redux';
 
 const authState = {
-    isLogin:true,
-    token:'',
-    isLoading:false,
-    isError:false,
-    alertMsg:""
-}
+  isLogin: true,
+  token: '',
+  isLoading: false,
+  isError: false,
+  alertMsg: '',
+};
 
 export default combineReducers({
-    auth:(state = authState,action)=>{
-        switch (action.type){
-            case 'LOGIN_PENDING':{
-                return{
-                    ...state,
-                    isLoading:true,
-                    isError:false,
-                    alertMsg:"Login ..."
-
-                }
-            }
-            case 'LOGIN_FULFILLED':{
-                return {
-                    ...state,
-                    isLoading:false,
-                    isLogin:true,
-                    isError:false,
-                    token:action.payload.data.token
- 
-                }
-            }
-            case'LOGIN_REJECTED':{
-                return{
-                ...state,
-                isLogin:false,
-                isError:true,
-                alertMsg:"Wrong email or password"
-            }
-            }
-            default:{
-                return state
-            }
-        }
+  auth: (state = authState, action) => {
+    switch (action.type) {
+      case 'LOGIN_PENDING': {
+        return {
+          ...state,
+          isLoading: true,
+          isError: false,
+          alertMsg: 'Login ...',
+        };
+      }
+      case 'LOGIN_FULFILLED': {
+        return {
+          ...state,
+          isLoading: false,
+          isLogin: true,
+          isError: false,
+          token: action.payload.data.token,
+        };
+      }
+      case 'LOGIN_REJECTED': {
+        return {
+          ...state,
+          isLogin: false,
+          isError: true,
+          alertMsg: 'Wrong email or password',
+        };
+      }
+      default: {
+        return state;
+      }
     }
-})
+  },
+});
