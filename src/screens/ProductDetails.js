270b099021f889e41e28a965f674730606f8e556
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-16 09:54:36 +0800

    created Product details screens

---
+++ src/screens/ProductDetails.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function OrderDetails() {
+  return (
+    <View>
+      <Text>Product Details</Text>
+    </View>
+  );
+}
